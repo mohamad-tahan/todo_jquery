a926ba5dad43c3f1685981984dde592a028a6e6e
mohamad-tahan <mohamadtahan880@gmail.com>
2022-06-02 15:44:36 +0300

    Allowing edit button to change values

--- scripts/todo.js
+++ scripts/todo.js
@@ -8,7 +8,7 @@ emptyNewRow = emptyNewRow + "<td class='tdDescription'><input type='text' class=
    
 emptyNewRow = emptyNewRow + "<td class='tdPoint'><select class='txtPoint' name='points' placeholder='Enter Point'><option value='1'>1</option><option value='2'>2</option><option value='3'>3</option><option value='4'>4</option><option value='5'>5</option></select></td>";
 
-emptyNewRow = emptyNewRow + "<td class='tdIsDone'><input type='checkbox' class='txtIsDone' name='True' value='Yes'><label for='True'> Yes </label><input type='checkbox' class='txtIsDone' name='False' value='No'><label for='False'> No </label> </td>";    
+emptyNewRow = emptyNewRow + "<td class='tdIsDone'><input type='checkbox' class='txtIsDone' name='True' value='No'><label for='True'> Yes </label><input type='checkbox' class='txtIsDone' name='False' value='No'><label for='False'> No </label> </td>";    
 
 emptyNewRow = emptyNewRow + "<td class='tdCreatedAt'><input type='datetime-local' class='txtCreatedAt' placeholder='Enter CreatedAt'/></td>";
 
@@ -56,3 +56,23 @@ $('#data').on('click', '.btnCancel', function () {
     $(this).parent().parent().remove();
 });
  
+
+$('#data').on('click', '.btnEdit', function () { 
+    const title =$(this).parent().parent().find(".tdTitle").html();
+    $(this).parent().parent().find(".tdTitle").html("<input type='text' value='"+title+"' class='txtTitle' placeholder='Enter Title'/>"); 
+
+    const description =$(this).parent().parent().find(".tdDescription").html();
+    $(this).parent().parent().find(".tdDescription").html("<input type='text' value='"+description+"' class=txtDescription' placeholder='Enter Description'/>"); 
+
+    const point =$(this).parent().parent().find(".tdPoint").html();
+    $(this).parent().parent().find(".tdPoint").html("<select class='txtPoint' value = '"+point+"' name='points' placeholder='Enter Point'><option value='1'>1</option><option value='2'>2</option><option value='3'>3</option><option value='4'>4</option><option value='5'>5</option></select>"); 
+
+    const isDone =$(this).parent().parent().find(".tdIsDone").html();
+    $(this).parent().parent().find(".tdIsDone").html("<input type='checkbox' class='txtIsDone' name='True' value='Yes'><label for='True'> Yes </label><input type='checkbox' class='txtIsDone' name='False' value='No'><label for='False'> No </label>"); 
+
+    const createdAt =$(this).parent().parent().find(".tdCreatedAt").html();
+    $(this).parent().parent().find(".tdCreatedAt").html("<input type='datetime-local' value = '"+createdAt+"'class='txtCreatedAt' placeholder='Enter CreatedAt'/>"); 
+
+
+    
+});
