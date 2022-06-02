92e38a0862550a66262ffc313842ff6f2f157aa2
mohamad-tahan <mohamadtahan880@gmail.com>
2022-06-02 23:45:42 +0300

    Changing background color of done todos

--- scripts/todo.js
+++ scripts/todo.js
@@ -8,7 +8,7 @@ emptyNewRow = emptyNewRow + "<td class='tdDescription'><input type='text' class=
    
 emptyNewRow = emptyNewRow + "<td class='tdPoint'><select class='txtPoint' name='points' placeholder='Enter Point'><option value='1'>1</option><option value='2'>2</option><option value='3'>3</option><option value='4'>4</option><option value='5'>5</option></select></td>";
 
-emptyNewRow = emptyNewRow + "<td class='tdIsDone'><select class='txtIsDone' name='IsDone' placeholder='Enter Is Done'><option value='Yes'>Yes</option><option value='No'>No</option></select></td>";    
+emptyNewRow = emptyNewRow + "<td class='tdIsDone'><select class='txtIsDone' name='IsDone' placeholder='Enter Is Done'><option value='No'>No</option><option value='Yes'>Yes</option></select></td>";    
 
 emptyNewRow = emptyNewRow + "<td class='tdCreatedAt'><input type='datetime-local' class='txtCreatedAt' placeholder='Enter CreatedAt'/></td>";
 
@@ -46,6 +46,19 @@ $('#data').on('click', '.btnSave', function () {
     const isdone =  $(this).parent().parent().find(".txtIsDone").val();
     $(this).parent().parent().find(".tdIsDone").html(isdone);
     console.log(isdone);
+    
+
+    //changind background color 
+    if(isdone === "Yes"){
+        $(this).parent().parent().find(".tdID").css("backgroundColor", "gray");
+        $(this).parent().parent().find(".tdTitle").css("backgroundColor", "gray");
+        $(this).parent().parent().find(".tdDescription").css("backgroundColor", "gray");
+        $(this).parent().parent().find(".tdPoint").css("backgroundColor", "gray");
+        $(this).parent().parent().find(".tdIsDone").css("backgroundColor", "gray");
+        $(this).parent().parent().find(".tdCreatedAt").css("backgroundColor", "gray");
+        $(this).parent().parent().find(".tdAction").css("backgroundColor", "gray");
+
+    }
 
     const createdat =  $(this).parent().parent().find(".txtCreatedAt").val();
     $(this).parent().parent().find(".tdCreatedAt").html(createdat);
@@ -77,7 +90,7 @@ $('#data').on('click', '.btnEdit', function () {
     $(this).parent().parent().find(".tdPoint").html("<select class='txtPoint' value = '"+point+"' name='points' placeholder='Enter Point'><option value='1'>1</option><option value='2'>2</option><option value='3'>3</option><option value='4'>4</option><option value='5'>5</option></select>"); 
 
     const isDone =$(this).parent().parent().find(".tdIsDone").html();
-    $(this).parent().parent().find(".tdIsDone").html("<select class='txtIsDone' value = '"+isDone+"' name='IsDone' placeholder='Enter Is Done'><option value='Yes'>Yes</option><option value='No'>No</option></select>");
+    $(this).parent().parent().find(".tdIsDone").html("<select class='txtIsDone' value = '"+isDone+"' name='IsDone' placeholder='Enter Is Done'><option value='No'>No</option><option value='Yes'>Yes</option></select>");
 
     const createdAt =$(this).parent().parent().find(".tdCreatedAt").html();
     $(this).parent().parent().find(".tdCreatedAt").html("<input type='datetime-local' value = '"+createdAt+"'class='txtCreatedAt' placeholder='Enter CreatedAt'/>"); 
