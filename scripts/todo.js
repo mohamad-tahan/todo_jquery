66823b4e641f6cd92c3672cf60b6d9bbf20fb0c5
mohamad-tahan <mohamadtahan880@gmail.com>
2022-06-02 23:29:51 +0300

    Adding and updatind data into local storage

--- scripts/todo.js
+++ scripts/todo.js
@@ -8,7 +8,7 @@ emptyNewRow = emptyNewRow + "<td class='tdDescription'><input type='text' class=
    
 emptyNewRow = emptyNewRow + "<td class='tdPoint'><select class='txtPoint' name='points' placeholder='Enter Point'><option value='1'>1</option><option value='2'>2</option><option value='3'>3</option><option value='4'>4</option><option value='5'>5</option></select></td>";
 
-emptyNewRow = emptyNewRow + "<td class='tdIsDone'><input type='checkbox' class='txtIsDone' name='True' value='Yes'><label for='True'> Yes </label><input type='checkbox' class='txtIsDone' name='False' value='No' checked><label for='False'> No </label> </td>";    
+emptyNewRow = emptyNewRow + "<td class='tdIsDone'><select class='txtIsDone' name='IsDone' placeholder='Enter Is Done'><option value='Yes'>Yes</option><option value='No'>No</option></select></td>";    
 
 emptyNewRow = emptyNewRow + "<td class='tdCreatedAt'><input type='datetime-local' class='txtCreatedAt' placeholder='Enter CreatedAt'/></td>";
 
@@ -17,7 +17,7 @@ emptyNewRow = emptyNewRow + "<td class='tdAction'><button class='btnSave'>Save</
 
 var editDeleteButtons ="<button class='btnEdit'>Edit</button> <button class='btnDelete'>Delete</button> ";
 var updateCancelButtons ="<button class='btnSave'> Update </button>  <button class='btnSave'> Cancel </button> ";
-var array = [];
+
 
 
 $("#btnAdd").click(function () { 
@@ -45,11 +45,17 @@ $('#data').on('click', '.btnSave', function () {
 
     const isdone =  $(this).parent().parent().find(".txtIsDone").val();
     $(this).parent().parent().find(".tdIsDone").html(isdone);
+    console.log(isdone);
 
     const createdat =  $(this).parent().parent().find(".txtCreatedAt").val();
     $(this).parent().parent().find(".tdCreatedAt").html(createdat);
 
     $(this).parent().parent().find(".tdAction").html(editDeleteButtons);
+
+      //Storing data in local storage
+      var array = [id,title, description,point, isdone];
+      console.log(array);
+      localStorage.setItem("array", JSON.stringify(array));
 
    
 
@@ -71,12 +77,19 @@ $('#data').on('click', '.btnEdit', function () {
     $(this).parent().parent().find(".tdPoint").html("<select class='txtPoint' value = '"+point+"' name='points' placeholder='Enter Point'><option value='1'>1</option><option value='2'>2</option><option value='3'>3</option><option value='4'>4</option><option value='5'>5</option></select>"); 
 
     const isDone =$(this).parent().parent().find(".tdIsDone").html();
-    $(this).parent().parent().find(".tdIsDone").html("<input type='checkbox' class='txtIsDone' name='True' value='Yes'><label for='True'> Yes </label><input type='checkbox' class='txtIsDone' name='False' value='No'><label for='False'> No </label>"); 
+    $(this).parent().parent().find(".tdIsDone").html("<select class='txtIsDone' value = '"+isDone+"' name='IsDone' placeholder='Enter Is Done'><option value='Yes'>Yes</option><option value='No'>No</option></select>");
 
     const createdAt =$(this).parent().parent().find(".tdCreatedAt").html();
     $(this).parent().parent().find(".tdCreatedAt").html("<input type='datetime-local' value = '"+createdAt+"'class='txtCreatedAt' placeholder='Enter CreatedAt'/>"); 
 
     $(this).parent().parent().find(".tdAction").html(updateCancelButtons);
+
+    //updating local storage
+    var array = [id,title, description,point, isdone];
+      console.log(array);
+      localStorage.setItem("array", JSON.stringify(array));
+
+
     
 
 });
